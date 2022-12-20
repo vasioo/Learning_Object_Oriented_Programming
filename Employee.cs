1972b4fb361ac8a88ceb1baa7c9dabe9fc5f5adb
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 17:04:44 +0200

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Employee
+    {
+        public Employee(int salary,int experience)
+        {
+            Salary = salary;
+            Experience = experience;
+        }
+        public decimal Salary { get; set; }
+        public int WorkHours { get; set; }
+        public int Experience { get; set; }
+    }
+}
