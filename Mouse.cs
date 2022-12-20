3c6d9f4a0a4b8ce017948e6557b9a2d00bbafd85
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 17:07:45 +0200

    Add files via upload

---
+++ Mouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Virtual
+{
+    class Mouse:Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Mouse eating");
+            Food -= 1;
+        }
+    }
+}
