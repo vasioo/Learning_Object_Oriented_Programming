70f16e4568285dd030a17b2a118f8b73c093c038
Vasil <vaskoorakov17@gmail.com>
2022-12-20 16:56:10 +0200

    added constructors exercises

---
+++ Abstraction/Constructor/Mammal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors
+{
+    class Mammal:Animal
+    {
+        public Mammal(int years):base(10)
+        {
+            Console.WriteLine("Mammal constructor");
+        }
+        public int EatAmount { get; set; }
+        public void Eat()
+        {
+            Console.WriteLine("Mammal eating");
+        }
+        public void MammalEat(int EatAmount)
+        {
+            this.Eat();
+            base.Eat();
+        }
+
+    }
+}
---
+++ Abstraction/Constructor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Mammal mammal = new Mammal(5);
+            mammal.MammalEat(5);
+        }
+    }
+}
