a5a2d0a07eceba4d1d5eebad8d9e1e80aa916cd5
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 17:18:26 +0200

    Add files via upload

---
+++ IDrivable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    interface IDrivable
+    {
+        public void Refuel(double litres);
+        public double FuelQuantity { get; set; }
+        public double FuelConsumptionPerKm { get; set; }
+        public double TankCapacity { get; set; }
+    }
+}
