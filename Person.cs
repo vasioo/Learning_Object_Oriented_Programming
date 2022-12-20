70e524c39a38ad7ef9a660745cb7368d8f74061c
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 16:18:45 +0200

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceExercises
+{
+    public class Person
+    {
+        public Person(string name,int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format($"Name: {this.Name}, Age: {this.Age}"));
+            return stringBuilder.ToString();
+        }
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
