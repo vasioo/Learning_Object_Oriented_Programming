b1c0cd8d2cef0e775452b18c52b3ed6392a17435
Vasil <vaskoorakov17@gmail.com>
2022-12-20 16:59:53 +0200

    added a bank exercise

---
+++ Abstraction/Bank/IAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mocking_And_TDD.Contracts
+{
+    public interface IAccount
+    {
+        void DepositMoney(decimal amount);
+        void WithdrawMoney(decimal amount);
+        public decimal Amount { get; }
+    }
+}
