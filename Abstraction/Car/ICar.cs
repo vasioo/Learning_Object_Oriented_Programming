8448ef57c9922c4d5c7e0fca7618373622648d67
Vasil <vaskoorakov17@gmail.com>
2022-12-20 16:54:49 +0200

    added abstraction exercises

---
+++ Abstraction/Car/ICar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    interface ICar
+    {
+        string Start();
+        string Stop();
+        string Color { get; set; }
+        string Model { get; set; }
+    }
+}
---
+++ Abstraction/Car/IEelectricCar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    interface IEelectricCar
+    {
+        int Battery { get; set; }
+    }
+}
