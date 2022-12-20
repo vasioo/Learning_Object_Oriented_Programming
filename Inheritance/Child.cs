511091c6e3d0ecccf4881ee628f2de762d3dee85
Vasil <vaskoorakov17@gmail.com>
2022-12-20 16:21:19 +0200

    used inheritance

---
+++ Inheritance/Child.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceExercises
+{
+    public class Child: Person
+    {
+        public Child(string name,int age):base(name,age)
+        {
+
+        }
+    }
+}
---
+++ Inheritance/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Gorilla gorilla = new Gorilla("a");
+            Console.WriteLine(gorilla);
+        }
+    }
+}
