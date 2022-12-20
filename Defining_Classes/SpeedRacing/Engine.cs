deb058e3c8cd5bd32d5098982907251dab4c6ba6
Vasil <vaskoorakov17@gmail.com>
2022-12-20 16:50:27 +0200

    added defined classes folder

---
+++ Defining_Classes/SpeedRacing/Engine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        public Engine(int horsePower, int cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+        public int HorsePower { get; set; }
+        public double CubicCapacity{ get; set; }
+    }
+}
