c878d93049195babb6b82105e760007f230dea91
Vasil <vaskoorakov17@gmail.com>
2022-12-20 17:07:16 +0200

    added a new exercise

---
+++ Inheritance/InheritanceExercises/RandomList/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("Pesho1");
+            list.Add("Pesho2");
+            list.Add("Pesho3");
+            list.Add("Pesho4");
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
