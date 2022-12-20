a3acdf3d5e62ec7cd098692d49ca6a48272c9e4b
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 16:55:27 +0200

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors
+{
+    class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Animal constructor");
+        }
+        public Animal(int years)
+        {
+            Console.WriteLine("Animal ctor with years");
+        }
+        public int EatAmount { get; set; }  
+        public void Eat()
+        {
+            Console.WriteLine("I am eating");
+        }
+    }
+}
---
+++ Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors
+{
+    class Dog : Mammal
+    {
+        public Dog():base(5)
+        {
+            Console.WriteLine("Dog constructor");
+        }
+    }
+}
