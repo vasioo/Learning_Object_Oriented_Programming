7b00af77602bbe398c95b6913f2b04e965b3cf02
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 17:09:51 +0200

    Add files via upload

---
+++ Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthorProblem
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method,AllowMultiple =true)]
+    public class Author : Attribute
+    {
+        public string Name { get; set; }
+        public Author(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Tracker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            foreach (var method in type.GetMethods())
+            {
+                object[] attrs = method.GetCustomAttributes(false);
+                foreach (var attr in attrs)
+                {
+                    Author authorAttr = attr as Author;
+                    if (authorAttr!=null)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {authorAttr.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
